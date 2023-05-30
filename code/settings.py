15e8d3b4c37eaa63f6c71d8e36a0eb8d20dddc7f
KrammerMat <114938199+KrammerMat@users.noreply.github.com>
2023-05-30 21:02:12 +0200

    Add files via upload

---
+++ code/settings.py
@@ -0,0 +1,8 @@
+# set screen height and width
+
+WINDOW_HEIGHT = 480
+WINDOW_WIDTH  = 800
+FRAMERATE = 120
+
+# separate file for settings, in order to find the code more easily, if we
+# wanted to adjust the size for the gaming window
